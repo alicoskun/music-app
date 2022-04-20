5e3b2ea7decc9b244fd26caec295509cf99ff114
alicoskun <ali.coskun1@outlook.com>
2022-04-20 23:41:45 +0300

    Design player layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,53 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import GradientLayout from "../components/gradientLayout"
+import prisma from "../lib/prisma"
+import { Box, Flex, Text } from "@chakra-ui/layout"
+import { Image } from "@chakra-ui/react"
 
-export default function Home() {
-  return <div>home</div>
+const Home = ({ artists }) => {
+  return (
+    <GradientLayout
+      roundImage
+      color="purple"
+      subtitle="profile"
+      title="Ali Coşkun"
+      description="15 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+    >
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to toy</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%" key={artist.id}>
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                  marginX="auto"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </GradientLayout>
+  )
 }
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: { artists },
+  }
+}
+
+export default Home
